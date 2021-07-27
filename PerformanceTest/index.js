b19389b922565156b77cf22d3e652b20f710c00a
hieu2010 <huyvietnguyen@hotmail.de>
2021-07-27 13:49:02 +0200

    test update

--- PerformanceTest/index.js
+++ PerformanceTest/index.js
@@ -1,35 +1,41 @@
 const fetch = require('node-fetch');
 
-var trackingQuery = `query getDailyActivity($purpose: String!){
-    DailyActivity(Purpose: $purpose, Id: 8877689391) {
-      Id
-      ActivityDate
+var trackingQuery = `query getDailyActivity($Purpose: String!){
+    DailyActivity(Purpose: $Purpose, Id: 8877689391) {
+        Id,
+        ActivityDate
     }
   }`;
 
 function sendQuery(query, i) {
     if(i<1000) {
         fetch('http://localhost:4000', {
-    method: 'POST',
-    headers: {
-        'Content-Type': 'application/json',
-        'Accept': 'application/json',
-    },
-    body: JSON.stringify({
-        query,
-        variables: {
-            purpose: "health"
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+            'Accept': 'application/json',
         },
-    })
-    })
-    .then(r => r.json())
-    .then(data => {
-        sendQuery(i+1);
-    });
+        body: JSON.stringify({
+            query,
+            variables: {
+                Purpose: "prescriptive sleep analytics"
+            },
+        })
+        })
+        .then(r => r.json())
+        .then(data => {
+            if(i % 100 == 0) process.stdout.write("#");
+            sendQuery(query, i+1);
+        });
     } else {
+        console.log();
+        console.log("----------");
+        console.log();
 		console.log("Test#1: Done.");
 	}
 }
 
+console.log();
 console.log("Test#1: Running..");
+console.log("----------");
 sendQuery(trackingQuery, 0);
