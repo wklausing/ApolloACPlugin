d40653d04e96a279ba771703a5b700d7297d9a6d
hieu2010 <huyvietnguyen@hotmail.de>
2021-07-27 21:56:21 +0200

    test additional parameter

--- PerformanceTest/index.js
+++ PerformanceTest/index.js
@@ -3,42 +3,48 @@ let fs = require('fs');
 const log = require('simple-node-logger').createSimpleFileLogger('response.log');
 log.setLevel('info');
 
+if (process.argv.length == 3) {
+    if (!isNaN(process.argv[2])) {
+        var testNumber = process.argv[2];
+        var query = fs.readFileSync('test' + testNumber + '.txt', 'utf8');
 
-var testNumber = 3;
-var query = fs.readFileSync('test' + testNumber + '.txt', 'utf8');
+        function sendQuery(query, purpose, i) {
+            if (i < 1000) {
+                fetch('http://localhost:4000', {
+                    method: 'POST',
+                    headers: {
+                        'Content-Type': 'application/json',
+                        'Accept': 'application/json',
+                    },
+                    body: JSON.stringify({
+                        query,
+                        variables: {
+                            Purpose: purpose
+                        },
+                    })
+                })
+                    .then(r => r.json())
+                    .then(data => {
+                        if (i == 0) log.info(JSON.stringify(data, null, 2));
+                        if (i % 100 == 0) process.stdout.write("#");
+                        sendQuery(query, purpose, i + 1);
+                    });
+            } else {
+                console.log();
+                console.log("----------");
+                console.log();
+                console.log("Test #" + testNumber + ": Done.");
+            }
+        }
 
-function sendQuery(query, purpose, i) {
-    if (i < 1000) {
-        fetch('http://localhost:4000', {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json',
-                'Accept': 'application/json',
-            },
-            body: JSON.stringify({
-                query,
-                variables: {
-                    Purpose: purpose
-                },
-            })
-        })
-            .then(r => r.json())
-            .then(data => {
-                if (i == 0) log.info(JSON.stringify(data, null, 2));
-                if (i % 100 == 0) process.stdout.write("#");
-                sendQuery(query, purpose, i + 1);
-            });
-    } else {
         console.log();
+        console.log("Test #" + testNumber + ": Running..");
         console.log("----------");
-        console.log();
-        console.log("Test #" + testNumber + ": Done.");
+
+        sendQuery(query, "track activity", 0);
+    } else {
+        console.log("Please use a number as additional parameter");
     }
+} else {
+    console.log("Please use an additional <integer> parameter")
 }
-
-console.log();
-console.log("Test #" + testNumber + ": Running..");
-console.log("----------");
-
-sendQuery(query, "track activity", 0);
-
